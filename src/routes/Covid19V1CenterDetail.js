ab1f8ed772ef8203d07b70e153c9947384ff853f
LEEDAESUNG <bitkorea21c@naver.com>
2022-11-14 23:58:17 +0900

    App SwiperSlide 추가

--- src/routes/Covid19V1CenterDetail.js
+++ src/routes/Covid19V1CenterDetail.js
@@ -3,19 +3,27 @@
 // ex) https://api.odcloud.kr/api/15077586/v1/centers?page=1&perPage=10000&serviceKey=76b%2FmW%2BbHOr2a8ibUv5RGW%2Bq1mDsJAagvGOrhhO%2FDbrczAJrsf50sC1zxpnjz6uBaS%2BaCbR07yKV4lj%2F3BFKAQ%3D%3D
 
 //네이버 지도 이용위한 설치
-//npm i -D @types/navermaps
+//1.npm i -D @types/navermaps
+//2.index.html 에 아래 script 추가
+//  <script type="text/javascript" src="https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=ohxl1bxta0"></script>
 
 import { useEffect, useState, useInterval, useRef } from "react";
 import { useParams } from "react-router-dom";
 import { Nav, Tab } from "react-bootstrap";
 import { useDispatch, useSelector } from 'react-redux';
 import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 
 function Covid19V1CenterDetail(props) {
 
 //"https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=ohxl1bxta0"
     
     const mapElement = useRef(null);
+    const location = useLocation();
+    const mapx = location.state.mapx;
+    const mapy = location.state.mapy;
+    console.log(mapx);
+    console.log(mapy);
 
 
     useEffect(() => {
@@ -23,7 +31,7 @@ function Covid19V1CenterDetail(props) {
         if (!mapElement.current || !naver) return;
 
         // 지도에 표시할 위치의 위도와 경도 좌표를 파라미터로 넣어줍니다.
-        const location = new naver.maps.LatLng(37.5656, 126.9769);
+        const location = new naver.maps.LatLng(mapx, mapy);
         const mapOptions: naver.maps.MapOptions = {
             center: location,
             zoom: 17,
@@ -40,7 +48,11 @@ function Covid19V1CenterDetail(props) {
     }, []);
 
 
-    return <div ref={mapElement} style={{ minHeight: '400px' }} />;
+    return (
+        <>
+            <div ref={mapElement} style={{ minHeight: '400px' }} /> 
+        </>
+    )
 }
 
 export default Covid19V1CenterDetail;
